8bb4be83b7e0eea4ef6803d98bbad553bd17f395
rjtt17 <rjtt17@gmail.com>
2021-01-18 09:46:08 +0900

    フォロー機能実装のために追加

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class RelationshipsController < ApplicationController
+  before_action :set_user, only: %i[create destroy]
+
+  def create
+    current_user.follow(@user)
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    current_user.unfollow(@user)
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:relationship][:following_id])
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,25 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show followings followers]
+
   def index
     @users = User.order(:id).page(params[:page])
   end
 
-  def show
-    @user = User.find(params[:id])
+  def show; end
+
+  def followings
+    @users = @user.followings
+  end
+
+  def followers
+    @users = @user.followers
+  end
+
+  private
+
+  def set_user
+    @user  = User.find(params[:id])
   end
 end
