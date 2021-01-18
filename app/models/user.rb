27dd5b7e9c9fdfb29f933fb2f0fa1301fc3cfe71
rjtt17 <rjtt17@gmail.com>
2021-01-18 09:43:10 +0900

    フォロー機能実装のために追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,11 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: :follower_id, dependent: :destroy, inverse_of: :follower
+  has_many :followers, through: :passive_relationships, source: :following
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: :following_id, dependent: :destroy, inverse_of: :following
+  has_many :followings, through: :active_relationships, source: :follower
+
   validates :uid, uniqueness: { scope: :provider }, if: -> { uid.present? }
 
   def self.from_omniauth(auth)
@@ -15,5 +20,17 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
     end
+  end
+
+  def follow(other_user)
+    followings << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(following_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    followings.include?(other_user)
   end
 end
