b359c6dd08bca7761232b7e8b5ff3ce5c5c45038
rjtt17 <rjtt17@gmail.com>
2021-01-19 22:32:59 +0900

    relationshipsテーブルの変更

---
+++ db/migrate/20210119132608_change_relationships_followrid_and_followingid_not_null.rb
@@ -0,0 +1,6 @@
+class ChangeRelationshipsFollowridAndFollowingidNotNull < ActiveRecord::Migration[6.1]
+  def change
+    change_column_null :relationships, :follower_id, false
+    change_column_null :relationships, :following_id, false
+  end
+end
