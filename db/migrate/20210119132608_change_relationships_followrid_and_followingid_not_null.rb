633c22636203484d6f8882ed69d6d3cccf6c4d53
rjtt17 <rjtt17@gmail.com>
2021-01-20 22:13:58 +0900

    リモートからマージした部分の修正

--- db/migrate/20210119132608_change_relationships_followrid_and_followingid_not_null.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class ChangeRelationshipsFollowridAndFollowingidNotNull < ActiveRecord::Migration[6.1]
-  def change
-    change_column_null :relationships, :follower_id, false
-    change_column_null :relationships, :following_id, false
-  end
-end
