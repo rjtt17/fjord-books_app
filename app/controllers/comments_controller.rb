fc6a822969c74e2a3a4dabf72c47016032919464
rjtt17 <rjtt17@gmail.com>
2021-02-10 09:05:11 +0900

    ストロングパラメータの修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -45,6 +45,6 @@ class CommentsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def comment_params
-    params.require(:comment).permit(:body, :user_id, :commentable_id, :commentable_type)
+    params.require(:comment).permit(:body)
   end
 end
