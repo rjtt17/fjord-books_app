5de951aecf7e315fe77a5a7adc5c736c206d1bfb
rjtt17 <rjtt17@gmail.com>
2021-02-14 15:07:05 +0900

    時刻の記述を修正

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,6 @@ module BooksApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.time_zone = 'Tokyo'
+    config.time_zone = 'Asia/Tokyo'
   end
 end
