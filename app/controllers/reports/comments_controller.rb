f7cbb24561e9fac84914ba212d513cc5cf2c0d6e
rjtt17 <rjtt17@gmail.com>
2021-02-14 21:18:53 +0900

    rubocopのエラー修正

--- app/controllers/reports/comments_controller.rb
+++ app/controllers/reports/comments_controller.rb
@@ -4,6 +4,7 @@ class Reports::CommentsController < CommentsController
   before_action :set_commentable
 
   private
+
   def set_commentable
     @commentable = Report.find(params[:report_id])
   end
