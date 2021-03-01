1ca1e4ea3863543b8db7ffbfdfd202f9ae64377a
rjtt17 <rjtt17@gmail.com>
2021-03-01 21:45:29 +0900

    日付の表記を変更

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -5,7 +5,7 @@ require 'test_helper'
 class ReportTest < ActiveSupport::TestCase
   test '#created_on' do
     me = User.create!(email: 'me@example.com', password: 'passward')
-    report = me.reports.create!(title: '日報1', content: 'テストは楽しい', created_at: '1 Jan 2020 00:00:00.000000000')
+    report = me.reports.create!(title: '日報1', content: 'テストは楽しい', created_at: '2020-01-01'.in_time_zone)
     assert_equal Date.new(2020, 1, 1), report.created_on
   end
 
